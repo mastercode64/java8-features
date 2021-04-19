8efd413894d36bc16316c695c1100b8b436888fb
Marcos Fujihashi <marcosfujihashi@gmail.com>
2021-04-19 12:28:13 -0300

    added project structure

---
+++ src/test/java/StreamTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StreamTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(10, 5 + 5);
+    }
+
+
+}
